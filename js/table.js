e6799d74e58674ee1edf380ff945764c8ebba916
Ashley Ryan <ashley19@umbc.edu>
2013-06-25 15:20:43 -0400

    add a show all button

--- js/table.js
+++ js/table.js
@@ -149,6 +149,17 @@ function createClickers(){
 			
 			OWF.Eventing.publish("testChannel1", JSON.stringify(apple));
 		});
+		
+	d3.select('#reset')
+		.on('click', function(){
+			createTable(MIN,MAX);
+			d3.selectAll("th").attr("class","unsorted");
+			
+			apple = table.getTimes();
+			for (i = 0; i< apple.length; i++){ apple[i] = Date.parse(apple[i]);	}
+			
+			OWF.Eventing.publish("testChannel1", JSON.stringify(apple));
+		});
 }
 
 /*Get a range of data based on start and end params
@@ -173,9 +184,7 @@ function createHeaders(arr){
 				.text(arr[i])
 				.attr("id", i)
 				.attr("class", "unsorted");
-		
 	}
-	//h.style("background-image", "url('next_century.png')");
 }
 
 function sendData(){
