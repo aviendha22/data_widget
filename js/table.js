44afa5a04aed014793f3c054f69555ab8d5d1dac
Ashley Ryan <ashley19@umbc.edu>
2013-06-25 15:38:45 -0400

    test receive data

--- js/table.js
+++ js/table.js
@@ -229,12 +229,11 @@ d3.json('./raw_data.txt', function(text){
 		OWF.ready(sendData);
 	});
 	
-
-	
 	OWF.Eventing.subscribe("testChannel2", function(sender, msg){
 		//assuming msg looks like [a,b]
 		var range = msg.substring(1,msg.length - 1).split(',');
 		createTable(parseInt(range[0], 10),parseInt(range[1], 10));
+		$('#start').val(msg);
 	});
 	
 });
