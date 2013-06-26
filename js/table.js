ca742e8f15a99d89c23634ff81c3e2d63c029942
Ashley Ryan <ashley19@umbc.edu>
2013-06-26 10:18:21 -0400

    test

--- js/table.js
+++ js/table.js
@@ -232,6 +232,7 @@ d3.json('./raw_data.txt', function(text){
 		var range = msg.substring(1,msg.length - 1).split(',');
 		$('#start').val(range[0]);
 		$('#end').val(range[1]);
+		createTable((new Date(range[0]).getTime()), (new Date(range[1]).getTime()));
 	});
 
 });
