9396721476565830ab8740e98a17e6718f41f975
Ashley <aviendha2281@gmail.com>
2013-08-12 10:00:39 -0400

    assertion graph testing

---
+++ js/convert.js
@@ -0,0 +1,78 @@
+/** Converts a json objedt which contains the following key/value pairs:
+ * {
+ * 		"entity1": "false claims",
+ * 		"relationship": "aggravate",
+ * 		"entity2": "identity theft"
+ * }
+ *
+ * into two arrays, nodes and links, the proper format to use the force d3
+ * example:
+ * 
+ * nodes = [
+ * 		{"name": "false claims", "group":0},
+ * 		{"name": "identity theft", "group":1},
+ * 		{"name": "the core group of defendants", "group":1}
+ * ];
+ *
+ * links = [
+ * 		{"source":0, "target":1, "value":"aggravate"},
+ *		{"source":2, "target":3, "value":"file"},
+ *		{"source":4, "target":5, "value":"expand"},
+ * ];
+ *
+ * where the number after source and target point to the index location in
+ * the nodes array of the node to point to for this link
+ */
+
+function createArrays(msg){
+	
+	var nodes = [],
+    links = [];
+	
+	var count = 0;
+
+	var data = [];
+
+	data.push(msg);
+
+	for(var i = 0; i < data.length; i++){
+		var item = data[i];
+		console.log(item);
+		var ent1 = {
+			name: item.entity1,
+			group: count
+			//group:0
+		};
+
+		var ent2 = {
+			name: item.entity2,
+			group: -(count+1)
+			//group: 1
+		};
+
+		if(nodes.indexOf(JSON.stringify(ent1)) === -1) {
+			nodes.push(JSON.stringify(ent1));
+			count++;
+		}
+
+		if(nodes.indexOf(JSON.stringify(ent2)) === -1){
+			nodes.push(JSON.stringify(ent2));
+			count++;
+		}
+
+		var index1 = nodes.indexOf(JSON.stringify(ent1));
+		var index2 = nodes.indexOf(JSON.stringify(ent2));
+
+		var rel = {
+			source: index1,
+			target: index2,
+			value: item.relationship
+		};
+
+		links.push(rel);
+	}
+
+	for (i = 0; i < nodes.length; i++) {
+		nodes[i] = JSON.parse(nodes[i]);
+	}
+}
---
+++ js/graph.js
@@ -0,0 +1,98 @@
+// Code based on http://bl.ocks.org/mbostock/2706022
+// and http://stackoverflow.com/questions/8663844/add-text-label-onto-links-in-d3-force-directed-graph
+var url = 'http://everest-build:8081/';
+
+var width = 600,
+    height = 600;
+
+var color = d3.scale.category10();
+
+var link, node, linktext;
+
+var svg = d3.select("body").append("svg")
+	.attr("width", width)
+	.attr("height", height);
+
+owfdojo.addOnLoad(function() {
+	OWF.ready(function() {
+		OWF.Eventing.subscribe("com.nextcentury.everest.assertion_announcing.assertions", update);
+	});
+});
+
+var update = function(sender, msg) {
+	//$.getJSON(url + 'assertion/?callback=?', function(data){
+	//d3.json('./js/raw_data.json', function(data){
+	createArrays(msg);
+	console.log(nodes);
+
+	var force = d3.layout.force()
+		.nodes(nodes)
+		.links(links)
+		.size([width, height])
+		.linkDistance(100)
+		.charge(-1000)
+		.on("tick", tick)
+		.start();
+
+	link = svg.selectAll(".link")
+		.data(links)
+		.enter().append("line")
+		.attr("class", "link");
+
+	node = svg.selectAll(".node")
+		.data(nodes)
+		.enter().append("g")
+		.attr("class", "node")
+		.on("mouseover", mouseover)
+		.on("mouseout", mouseout)
+		.style("fill", function(d) {
+				var c = d.group < 0 ? 0 : 1;
+				return color(c); 
+				})
+	.call(force.drag);
+
+	linktext = svg.selectAll("g.linklabelholder").data(force.links());
+	linktext.enter().append("g").attr("class", "linklabelholder")
+		.append("text")
+		.attr("class", "linklabel")
+		.attr("dx", 1)
+		.attr("dy", "1em")
+		.attr("text-anchor", "middle")
+		.text(function(d) { return d.value});
+
+	node.append("circle")
+		.attr("r", 8);
+
+	node.append("text")
+		.attr("x", 12)
+		.attr("dy", ".35em")
+		.text(function(d) { return d.name; });
+
+};
+
+function tick() {
+	link
+		.attr("x1", function(d) { return d.source.x; })
+		.attr("y1", function(d) { return d.source.y; })
+		.attr("x2", function(d) { return d.target.x; })
+		.attr("y2", function(d) { return d.target.y; });
+
+	node
+		.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+	linktext.attr("transform", function(d) {
+			return "translate(" + (d.source.x + d.target.x) / 2 + "," 
+			+ (d.source.y + d.target.y) / 2 + ")"; });
+}
+
+function mouseover() {
+	d3.select(this).select("circle").transition()
+		.duration(750)
+		.attr("r", 16);
+}
+
+function mouseout() {
+	d3.select(this).select("circle").transition()
+		.duration(750)
+		.attr("r", 8);
+}
